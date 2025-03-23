0a19190853f2b66d19cb93c1f26380aa45fee106
Damir Arh <damir.arh@gmail.com>
2025-03-23 08:00:22 +0100

    Add project files.

---
+++ PollyResilienceWrapper/HttpResilienceTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.RateLimiting;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.RateLimiting;
+using Shouldly;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace PollyResilienceWrapper;
+
+public class HttpResilienceTests
+{
+    private const string RATE_LIMITING_PIPELINE_KEY = "rate-limiting-pipeline";
+    private const int MAX_REQUESTS_PER_SECOND = 5;
+
+    public class MyService(HttpClient httpClient)
+    {
+        public async Task<string> GetAsync()
+        {
+            return await httpClient.GetStringAsync("");
+        }
+    }
+
+    private WireMockServer server;
+    private ServiceProvider serviceProvider;
+
+    [SetUp]
+    public void Setup()
+    {
+        server = WireMockServer.Start();
+        server
+            .Given(Request.Create().WithPath("/").UsingGet())
+            .RespondWith(Response.Create().WithBody("Hello world!"));
+
+        var services = new ServiceCollection();
+        services
+            .AddHttpClient<MyService>(client =>
+            {
+                client.BaseAddress = new Uri(server.Urls[0]);
+            })
+            .AddResilienceHandler(
+                RATE_LIMITING_PIPELINE_KEY,
+                builder =>
+                {
+                    builder.AddRateLimiter(
+                        new SlidingWindowRateLimiter(
+                            new SlidingWindowRateLimiterOptions
+                            {
+                                PermitLimit = MAX_REQUESTS_PER_SECOND,
+                                SegmentsPerWindow = 10,
+                                Window = TimeSpan.FromSeconds(1)
+                            }
+                        )
+                    );
+                }
+            );
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        server.Stop();
+        server.Dispose();
+
+        serviceProvider.Dispose();
+    }
+
+    [Test]
+    public async Task InjectsPipelineIntoService()
+    {
+        var service = serviceProvider.GetRequiredService<MyService>();
+
+        var tasks = Enumerable.Range(0, 5).Select(_ => service.GetAsync()).ToArray();
+        await Task.WhenAll(tasks).ShouldNotThrowAsync();
+
+        await service.GetAsync().ShouldThrowAsync<RateLimiterRejectedException>();
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        await service.GetAsync().ShouldNotThrowAsync();
+    }
+}
---
+++ PollyResilienceWrapper/PollyDiWithPipelineProviderTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading.RateLimiting;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.RateLimiting;
+using Polly.Registry;
+using Shouldly;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace PollyResilienceWrapper
+{
+    public class PollyDiWithPipelineProviderTests
+    {
+        private const string RATE_LIMITING_PIPELINE_KEY = "rate-limiting-pipeline";
+        private const int MAX_REQUESTS_PER_SECOND = 5;
+
+        public class MyService(
+            HttpClient httpClient,
+            ResiliencePipelineProvider<string> pipelineProvider
+        )
+        {
+            private readonly ResiliencePipeline rateLimitingPipeline = pipelineProvider.GetPipeline(
+                RATE_LIMITING_PIPELINE_KEY
+            );
+
+            public async Task<string> GetAsync()
+            {
+                return await rateLimitingPipeline.ExecuteAsync(async cancellationToken =>
+                    await httpClient.GetStringAsync("", cancellationToken)
+                );
+            }
+        }
+
+        private WireMockServer server;
+        private ServiceProvider serviceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            server = WireMockServer.Start();
+            server
+                .Given(Request.Create().WithPath("/").UsingGet())
+                .RespondWith(Response.Create().WithBody("Hello world!"));
+
+            var services = new ServiceCollection();
+            services.AddHttpClient<MyService>(client =>
+            {
+                client.BaseAddress = new Uri(server.Urls[0]);
+            });
+            services.AddResiliencePipeline(
+                RATE_LIMITING_PIPELINE_KEY,
+                (builder, context) =>
+                {
+                    builder.AddRateLimiter(
+                        new SlidingWindowRateLimiter(
+                            new SlidingWindowRateLimiterOptions
+                            {
+                                PermitLimit = MAX_REQUESTS_PER_SECOND,
+                                SegmentsPerWindow = 10,
+                                Window = TimeSpan.FromSeconds(1)
+                            }
+                        )
+                    );
+                }
+            );
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Stop();
+            server.Dispose();
+
+            serviceProvider.Dispose();
+        }
+
+        [Test]
+        public async Task InjectsProviderIntoService()
+        {
+            var service = serviceProvider.GetRequiredService<MyService>();
+
+            var tasks = Enumerable.Range(0, 5).Select(_ => service.GetAsync()).ToArray();
+            await Task.WhenAll(tasks).ShouldNotThrowAsync();
+
+            await service.GetAsync().ShouldThrowAsync<RateLimiterRejectedException>();
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await service.GetAsync().ShouldNotThrowAsync();
+        }
+    }
+}
